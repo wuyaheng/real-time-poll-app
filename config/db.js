214f2f198ee2fae7f931caa38784b17a23460fef
wuyaheng <wuyaheng2016@gmail.com>
2020-06-06 00:13:04 -0400

    delete comments

--- config/db.js
+++ config/db.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect('')
-//     .then(() => console.log('MongoDB Connected'))
-//     .catch(err => console.log(err));
-
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
     useNewUrlParser: true,
     useFindAndModify: false
