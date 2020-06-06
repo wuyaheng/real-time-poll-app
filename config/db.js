a336bda528c2905f8f00be05bf1046e3310f77a6
wuyaheng <wuyaheng2016@gmail.com>
2020-06-05 23:55:46 -0400

    connect mongoose

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('')
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+// mongoose.Promise = global.Promise;
+// mongoose.connect('')
+//     .then(() => console.log('MongoDB Connected'))
+//     .catch(err => console.log(err));
 
 
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
 
